02c2714f805552b1ed94983a5b649506ea2a5d8f
marcusoftnet <marcusoft.net@gmail.com>
2013-01-17 20:56:19 +0100

    Fist couple of tests in place

---
+++ DiscoverNancy.Tests/DiscoverNancy.Tests/TestingNancyWithoutWebStuff.cs
@@ -0,0 +1,70 @@
+﻿using Nancy;
+using Nancy.Testing;
+using Xunit;
+
+namespace DiscoverNancy.Tests
+{
+    public class TestingNancyWithoutWebStuff
+    {
+        [Fact]
+        public void simplest_get_test()
+        {
+            // Arrange
+            var browser = new Browser(with => with.Module(new SimpleModule()));
+
+            // Act
+            var response = browser.Get("/");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public void simplest_post_test()
+        {
+            // Arrange
+            var browser = new Browser(with => with.Module(new SimpleModule()));
+            var username = "marcusoftnet";
+
+            // Act
+            var response = browser.Post("/login/", with =>
+            {
+                with.HttpRequest();
+                with.FormValue("Username", username);
+                with.FormValue("Password", "really?YouThoughtIWouldRevealThat?");
+            });
+
+            // Assert
+            response.ShouldHaveRedirectedTo("/login?error=true&username=" + username);
+        }
+
+
+        /// <summary>
+        /// Yeah - that's a complete web application class
+        /// Inside my testing class library... 
+        /// Nothing strange here
+        /// </summary>
+        public class SimpleModule : NancyModule
+        {
+            public SimpleModule()
+            {
+                Get["/"] = _ => { return HttpStatusCode.OK; };
+
+                Get["/login"] = _ => { return View["Login"]; };
+                Post["/login"] = parameters =>
+                                {
+                                    return HandleLogin(parameters);
+                                };
+            }
+
+            private Response HandleLogin(dynamic parameters)
+            {
+                if (parameters.Password != "supersecret")
+                    return Response.AsRedirect("/login?error=true&username=marcusoftnet");
+
+                return Response.AsRedirect("/?username=marcusoftnet");
+
+            }
+        }
+    }
+}
