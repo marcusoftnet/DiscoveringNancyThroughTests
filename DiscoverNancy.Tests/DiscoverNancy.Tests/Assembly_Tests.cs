4d5fdafcce37fca1d3e56bd36ee046c4746664d4
marcusoftnet <marcusoft.net@gmail.com>
2013-01-25 13:43:50 +0100

    Added tests for Assembly

---
+++ DiscoverNancy.Tests/DiscoverNancy.Tests/Assembly_Tests.cs
@@ -0,0 +1,28 @@
+﻿using DiscoverNancy.Web;
+using Nancy.Testing;
+using Xunit;
+
+namespace DiscoverNancy.Tests
+{
+    public class Assembly_Tests
+    {
+        [Fact]
+        public void should_be_able_to_load_an_assembly_without_referencing_it()
+        {
+            // Arrange a browser for just our module
+            // is using a type in an assembly that our tests
+            // not is referencing
+            var b = new Browser(with =>
+                                    {
+                                        with.Module<ModuleThatUseSimpleDependency>();
+                                        with.Assembly("ExternalDependencies");
+                                    });
+
+            // Act 
+            var r = b.Get("/simpleDep");
+
+            // Assert
+            Assert.Equal("Albert", r.Body.AsString());
+        }
+    }
+}
---
+++ DiscoverNancy.Web/ModuleThatUseSimpleDependency.cs
@@ -0,0 +1,16 @@
+﻿using ExternalDependencies;
+using Nancy;
+
+namespace DiscoverNancy.Web
+{
+    public class ModuleThatUseSimpleDependency : NancyModule
+    {
+        public ModuleThatUseSimpleDependency()
+        {
+            // Don't ever do this - just proving the point that 
+            // the assembly with SimpleDependency is not referenced by my tests
+            var dependency = new SimpleDependency();
+            Get["/simpleDep"] = p => { return dependency.GetAName(); };
+        }
+    }
+}
